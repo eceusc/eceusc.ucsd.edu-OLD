1eb6a62803f9d56f7ff57580bad2ea1d0749a19b
Glitch (eceusc-eceusc-ucsd-edu) <none>
2018-09-25 00:05:56 +0000

    testing change

--- src/Routes/DeStress.jsx
+++ src/Routes/DeStress.jsx
@@ -7,7 +7,7 @@ export default class DeStress extends React.Component {
     return (<div>
       <Container
         header="dEEstressers"
-        subheader="lol fix me"
+        subheader="lol fix mexx"
         content="ECE is a stressful major, therefore we have our dEEstresser events where you can temporarily leave behind stressful thoughts and simply have fun and make friends! Come in the beginning of the quarter to design and take away a ethernet cable for your new dorm. At the same time, make some electric motors to decorate your room and get your year movin’!"/>
     </div>);
   }
