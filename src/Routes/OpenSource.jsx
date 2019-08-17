8e1771de82b05633626f37728c089d4d0327af99
Bao <bghoang.ucsd.edu>
2019-08-17 13:41:09 -0700

    update member info

--- src/Routes/OpenSource.jsx
+++ src/Routes/OpenSource.jsx
@@ -1,22 +1,36 @@
-import * as React from 'react';
-import {Container} from './helpers';
-import members from './members';
+import * as React from "react";
+import { Container } from "./helpers";
+import members from "./members";
 
 export default class OpenSource extends React.Component {
-
   render() {
-    return (<div>
-    <Container
-        header="Open Source"
-        subheader=""
-        content="The goal of the Open Source Program is to provide ECE students (who are more hardware orientated) with the software skills necessary to compete in an increasingly software growing industry. In this program, you will meet throughout the quarter (approx. bi-weekly) to attend workshops that will strengthen your programming and software skills, including (but not limited to) the Python programming language and using Git/GitHub. The workshops will also culminate with you completing a software project accessible in your own GitHub repository that can be shown off to employers on your resume."
-        content2={
-          <div style={{fontSize: '1.5rem', textAlign:'center', marginTop:'22px'}}>
-            Sign up for the Winter 2019 Open Source Team{' '}
-            <a href="https://goo.gl/forms/iK5SKlx7A76RLNxD2" target="_blank">here!</a>
-          </div>
-        }
-        contacts={[members[2]]}/>
-    </div>);
+    return (
+      <div>
+        <Container
+          header="Project Teams"
+          subheader=""
+          content="The Project Teams Program will host 3 to 4 hands-on ECE projects 
+          for the year. There will be dedicated Project Leads for each project, who 
+          will guide the project progress and teach members necessary skills and knowledge. 
+          Our projects are designed to be beginner-friendly and suitable for all skill levels, 
+          as long as you’re willing to commit time to learning."
+          content2={
+            <div
+              style={{
+                fontSize: "1.5rem",
+                textAlign: "center",
+                marginTop: "22px"
+              }}
+            >
+              Sign up for the 2019-2020 Project Teams{" "}
+              <a href="https://goo.gl/forms/iK5SKlx7A76RLNxD2" target="_blank">
+                here!
+              </a>
+            </div>
+          }
+          contacts={[members[2], members[9]]}
+        />
+      </div>
+    );
   }
 }
