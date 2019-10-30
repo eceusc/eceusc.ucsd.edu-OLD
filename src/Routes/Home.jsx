589ad73e38566858b0e2137dd38100475af104c6
Alex Garcia <alexsebastian.garcia@gmail.com>
2019-10-30 16:20:52 -0700

    Update Home.jsx

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -84,7 +84,7 @@ export default class Home extends React.Component {
       <div class="ui grid">
         <div class="ui centered row">
           <h1 class="ui centered header">
-            ECE Undergraduate Student Council (test)
+            ECE Undergraduate Student Council
             <div
               class="ui divider"
               style={{ marginTop: ".25rem", marginBottom: ".5rem" }}
