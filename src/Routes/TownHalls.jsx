44addeffb44c76057698578eb8377ed52af30c4d
Branson Beihl <47403344+MissouriBoy@users.noreply.github.com>
2020-10-30 22:12:26 -0700

    Added town hall event
    Oops, forgot we also had a town hall.

--- src/Routes/TownHalls.jsx
+++ src/Routes/TownHalls.jsx
@@ -3,6 +3,23 @@ import { Container } from "./helpers";
 import members from "./members";
 import { Grid, Image } from "semantic-ui-react";
 
+const Slides = () => (
+  <React.Fragment>
+    <div class="ui row">
+      <div style={{ textAlign: "center" }}>
+        <iframe
+          title="slides"
+          src="slides/UCSD ECE USC Town Hall 10_27_2020.pdf"
+          width="575"
+          height="375"
+          frameborder="0"
+          scrolling="no"
+        />
+      </div>
+    </div>
+  </React.Fragment>
+);
+
 const PhotoAlign = () => (
   <Grid>
     <Grid.Row columns={2}>
@@ -28,10 +45,46 @@ export default class TownHalls extends React.Component {
           content2={
             <div
               style={{
+                fontSize: "1.5rem",
+                textAlign: "center",
                 marginTop: "20px"
               }}
             >
               <PhotoAlign />
+              <div class="ui divider" />
+                Previous ECE Town Halls
+            </div>
+          }
+          content3={
+            <div style={{
+              fontSize: "1.0rem",
+              textAlign: "center",
+              marginTop: "22px"
+            }}
+            >
+              <div className="ui centered header"
+                style={{
+                  fontSize: "1.0rem",
+                  marginBottom: "0px"
+                }}
+              >
+                Fall 2020 Town Hall
+              </div>
+              <div className="ui centered header"
+                style={{
+                  fontSize: "1.0rem",
+                  marginTop: "0px"
+                }}
+              >
+                <a
+                  href="https://drive.google.com/file/d/1PPK7AKiANea89wH6Diw53lmTjNMp7zDk/view?usp=sharing"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Video
+                </a>
+              </div>
+              <Slides />
             </div>
           }
           contacts={[members[2]]}
